747f6eae99c14d5ad28f5853d7420da824fb5a7a
Kim Olsen <pizslacker@users.noreply.github.com>
2017-07-31 17:20:31 +0200

    Updated copyright-year in conanalyzer.php

--- conanalyzer.php
+++ conanalyzer.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright 2010-2016 Kim Olsen <kim@pizslacker.org>
+/* Copyright 2010-2017 Kim Olsen <kim@pizslacker.org>
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
